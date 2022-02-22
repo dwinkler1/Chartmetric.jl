37ade9a32ff77f6ed2c2e67927f69843dfebde00
Documenter.jl <documenter@juliadocs.github.io>
2022-02-22 09:23:26 +0000

    build based on 5c31e4b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Chartmetric","category":"page"},{"location":"#Chartmetric","page":"Home","title":"Chartmetric","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Chartmetric.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Chartmetric]","category":"page"}]
+}
