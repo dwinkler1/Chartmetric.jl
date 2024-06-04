afe49c14e48dc021ef7a897199c1c5e7c4f787de
Daniel Winkler <daniel.winkler@s.wu.ac.at>
2024-06-04 11:01:56 +1000

    More stable ratelimit protection and dependency updates

--- src/helpers.jl
+++ src/helpers.jl
@@ -12,7 +12,7 @@ function parseresponse(response::HTTP.Messages.Response)
     bod = response.body
     code = response.status
     if code ∉ 200:399
-        ret_dict = Dict{String, Any}()
+        ret_dict = Dict{String,Any}()
         ret_dict["code"] = code
         ret_dict["url"] = response.request.target
         try
@@ -37,19 +37,21 @@ end
 function buildrequesturl(path)
     base = "https://api.chartmetric.com/api/"
     urls = join(path, '/')
-    url_req =  base * urls
+    url_req = base * urls
     return url_req
 end
 
 # Not exported
-function ratelimitprotect(response::HTTP.Messages.Response; verbose = false)
+function ratelimitprotect(response::HTTP.Messages.Response; verbose=false)
     headers = Dict(response.headers)
-    remaining = haskey(headers, "X-RateLimit-Remaining") ? parse(Int, headers["X-RateLimit-Remaining"]) : 0
-    if remaining < 1
-        resetin =  haskey(headers, "X-RateLimit-Reset") ? parse(Int, headers["X-RateLimit-Reset"]) - time() : 60
-        sleepfor = maximum([0.1, resetin])
-        #verbose && println("Protecting rate limit")
-        sleep((sleepfor + 1))
+    remaining = haskey(headers, "X-RateLimit-Remaining") ? tryparse(Int, headers["X-RateLimit-Remaining"]) : 0
+    if isnothing(remaining) || remaining < 1
+        resetin = haskey(headers, "X-RateLimit-Reset") ? tryparse(Int, headers["X-RateLimit-Reset"]) - time() : 1
+        resetin = isnothing(resetin) ? 1 : resetin
+        resetin < 0 && return nothing
+        sleepfor = max(0.1, resetin)
+        verbose && println("Protecting rate limit")
+        sleep((sleepfor + 0.01))
     end
     return nothing
 end
